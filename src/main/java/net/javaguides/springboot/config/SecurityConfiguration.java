03ee1e9c0809358c6ea373da1ddc7a335021f977
JStanczykowski <jakub899.ks@gmail.com>
2022-11-22 13:01:48 +0100

    login-register-security

--- src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
+++ src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 				.formLogin()
 				.loginPage("/login")
-				.defaultSuccessUrl("/index", true)
+				.defaultSuccessUrl("/worker", true)
 				.permitAll()
 				.and()
 				.logout()
